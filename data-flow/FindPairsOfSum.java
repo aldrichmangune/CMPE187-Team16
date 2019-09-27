984473a5406e119f8f42741793005bef7d45db16
aldrichmangune <aldrichmangune@gmail.com>
2019-09-27 13:02:14 -0700

    Added test cases for Data Flow

--- data-flow/FindPairsOfSum.java
+++ data-flow/FindPairsOfSum.java
@@ -4,7 +4,54 @@ public class FindPairsOfSum {
         /*Assume the array is given, and the only input from the user is K*/
         int[] givenArr = {1, 2, 7, 11, 6, 9, 5};
         String K = "12";
-        System.out.println(findPairs(givenArr, K));
+        System.out.println("Example solution: " + findPairs(givenArr, K) + "\n");
+
+        testCases();
+    }
+
+    public static void testCases() {
+        System.out.format("%s%16s%60s\n", "Test Case", "Expected", "Actual");
+        // Blank Array
+        String expected = "None of integer pairs that make the sum equal to 12.";
+        int[] testArr1 = {};
+        String testK = "12";
+        System.out.format("%s%58s%62s\n", "Blank Array", expected, findPairs(testArr1, testK));
+
+        // No pairs in the Array
+        expected = "None of integer pairs that make the sum equal to 12.";
+        int[] testArr2 = {1, 4, 5, 6, 3, 2};
+        testK = "12";
+        System.out.format("%s%55s%62s\n", "No valid pairs", expected, findPairs(testArr2, testK));
+
+        // Blank K
+        expected = "Input must be an Integer.";
+        int[] testArr3 = {1, 2, 7, 11, 6, 9, 5};
+        testK = "";
+        System.out.format("%s%35s%61s\n", "Blank K", expected, findPairs(testArr3, testK));
+
+        // Double K
+        expected = "Input must be an Integer.";
+        int[] testArr4 = {1, 2, 7, 11, 6, 9, 5};
+        testK = "12.0";
+        System.out.format("%s%29s%61s\n", "K is a double", expected, findPairs(testArr4, testK));
+
+        // K is a character/string
+        expected = "Input must be an Integer.";
+        int[] testArr5 = {-1, 2, 7, -11, 6, 9, 5};
+        testK = "This is not an integer";
+        System.out.format("%s%29s%61s\n", "K is a string", expected, findPairs(testArr5, testK));
+
+        // Negative K
+        expected = "(-1, -11).";
+        int[] testArr6 = {-1, 2, 7, -11, 6, 9, 5};
+        testK = "-12";
+        System.out.format("%s%17s%61s\n", "Negative K", expected, findPairs(testArr6, testK));
+
+        // 2 valid Pairs
+        expected = "(1, 4),(2, 3).";
+        int[] testArr7 = {1, 4, 7, 11, 6, 9, 5, 2, 3};
+        testK = "5";
+        System.out.format("%s%18s%60s\n", "2 Valid pairs", expected, findPairs(testArr7, testK));
     }
 
     public static String findPairs(int[] givenArr, String K) {                      //(1)(2)
@@ -27,7 +74,7 @@ public class FindPairsOfSum {
                 }
             }
             if (s.isEmpty()) {                                                      //(20)
-                s = "Non of integer pairs that make the sum equal to " + K + ".";   //(21)
+                s = "None of integer pairs that make the sum equal to " + K + ".";  //(21)
             }
         } catch (NumberFormatException ex) {
             return "Input must be an Integer";                                      //(23)
