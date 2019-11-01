162a3669053165731cd460fa99e8833a84327a57
ThuanChau22 <thuan.chau@sjsu.edu>
2019-11-01 13:21:19 -0700

    All test cases added

--- equivalent-class-testing/NextDate.java
+++ equivalent-class-testing/NextDate.java
@@ -1,103 +1,104 @@
 public class NextDate {
     public static void main(String[] args) {
-      equivalentClassTesting();
-      boundaryTesting();
-      randomTesting();
+        equivalentClassTesting();
+        boundaryTesting();
+        randomTesting();
     }
 
+    // Equivalent Class Testing
     public static void equivalentClassTesting() {
-        // Month: has 31 days, day: 30, year: common
+        // M1 / D3 / Y3
         String actual = nextDate(3, 30, 2017);
         String expected = "03/31/2017";
         System.out.println("Equivalent 1: " + checkEqual(actual, expected));
 
-        // Month: has 31 days, day: 31, year: common
+        // M1 / D4 / Y3
         actual = nextDate(3, 31, 2017);
         expected = "04/01/2017";
         System.out.println("Equivalent 2: " + checkEqual(actual, expected));
 
-        // Month: has 30 days, day: 30, year common
+        // M2 / D3 / Y3
         actual = nextDate(4, 30, 2017);
         expected = "05/01/2017";
         System.out.println("Equivalent 3: " + checkEqual(actual, expected));
 
-        // Month: has 30 days, day: 31, year: common
+        // M2 / D4 / Y3
         actual = nextDate(4, 31, 2017);
         expected = "Invalid Date Input.";
         System.out.println("Equivalent 4: " + checkEqual(actual, expected));
 
-        // Month: has 29 days, day: 28, year: leap-every 4 years
+        // M3 / D1 / Y1
         actual = nextDate(2, 28, 2016);
         expected = "02/29/2016";
         System.out.println("Equivalent 5: " + checkEqual(actual, expected));
 
-        // Month: has 29 days, day: 29, year: leap-every 4 years
+        // M3 / D2 / Y1
         actual = nextDate(2, 29, 2016);
         expected = "03/01/2016";
         System.out.println("Equivalent 6: " + checkEqual(actual, expected));
 
-        // Month: has 28 days, day: 28, year: common
+        // M3 / D1 / Y3
         actual = nextDate(2, 28, 2017);
         expected = "03/01/2017";
         System.out.println("Equivalent 7: " + checkEqual(actual, expected));
 
-        // Month: has 28 days, day: 29, year: common
+        // M3 / D2 / Y3
         actual = nextDate(2, 29, 2017);
         expected = "Invalid Date Input.";
         System.out.println("Equivalent 8: " + checkEqual(actual, expected));
 
-        // Month: has 29 days, day: 29, year: leap-divisible by 4
+        // M3 / D2 / Y2
         actual = nextDate(2, 29, 2000);
         expected = "03/01/2000";
         System.out.println("Equivalent 9: " + checkEqual(actual, expected));
 
-        // Month: has 29 days, day: 30, year: leap-divisible by 4
+        // M3 / D3 / Y2
         actual = nextDate(2, 30, 2000);
         expected = "Invalid Date Input.";
         System.out.println("Equivalent 10: " + checkEqual(actual, expected));
     }
-  
-  // Boundary testing
+
+    // Boundary testing
     public static void boundaryTesting() {
-        // Boundary test
         // M1
-        System.out.println("M1 test: " + checkEqual(nextDate(6,29,2016), "06/30/2016"));
+        System.out.println("M1 test: " + checkEqual(nextDate(6, 29, 2016), "06/30/2016"));
         // M2
-        System.out.println("M2 test: " + checkEqual(nextDate(7,31,2016), "08/01/2016"));
+        System.out.println("M2 test: " + checkEqual(nextDate(7, 31, 2016), "08/01/2016"));
         // M3 has 28 days
-        System.out.println("M3(28 days) test: " + checkEqual(nextDate(2,28,2001), "03/01/2001"));
+        System.out.println("M3(28 days) test: " + checkEqual(nextDate(2, 28, 2001), "03/01/2001"));
         // M3 has 29 days
-        System.out.println("M3(29 days) test: " + checkEqual(nextDate(2,28,2016), "02/29/2016"));
+        System.out.println("M3(29 days) test: " + checkEqual(nextDate(2, 28, 2016), "02/29/2016"));
         // M3 invalid date
-        System.out.println("M3(invalid date) test: " + checkEqual(nextDate(2,30,2005), "Invalid Date Input."));
+        System.out.println("M3(invalid date) test: " + checkEqual(nextDate(2, 30, 2005), "Invalid Date Input."));
         // D1 left bound
-        System.out.println("D1(left bound) test: " + checkEqual(nextDate(2,1,2011), "02/02/2011"));
+        System.out.println("D1(left bound) test: " + checkEqual(nextDate(2, 1, 2011), "02/02/2011"));
         // D1 right bound
-        System.out.println("D1(right bound) test: " + checkEqual(nextDate(2,28,2013), "03/01/2013"));
+        System.out.println("D1(right bound) test: " + checkEqual(nextDate(2, 28, 2013), "03/01/2013"));
         // D1 just above left bound
-        System.out.println("D1(jsut above left bound) test: " + checkEqual(nextDate(2,2,2014), "02/03/2014"));
+        System.out.println("D1(jsut above left bound) test: " + checkEqual(nextDate(2, 2, 2014), "02/03/2014"));
         // D1 just below right bound
-        System.out.println("D1(just below right bound) test: " + checkEqual(nextDate(2,27,2015), "02/28/2015"));
+        System.out.println("D1(just below right bound) test: " + checkEqual(nextDate(2, 27, 2015), "02/28/2015"));
         // D1 out of left bound
-        System.out.println("D1(out of left bound) test: " + checkEqual(nextDate(2,0,2002), "Invalid Date Input."));
+        System.out.println("D1(out of left bound) test: " + checkEqual(nextDate(2, 0, 2002), "Invalid Date Input."));
         // D1 out of right bound
-        System.out.println("D1(out of right bound) test: " + checkEqual(nextDate(2,29,2013), "Invalid Date Input."));
+        System.out.println("D1(out of right bound) test: " + checkEqual(nextDate(2, 29, 2013), "Invalid Date Input."));
         // D2
-        System.out.println("D2 test: " + checkEqual(nextDate(2,29,2012), "03/01/2012"));
+        System.out.println("D2 test: " + checkEqual(nextDate(2, 29, 2012), "03/01/2012"));
         // D3
-        System.out.println("D3 test: " + checkEqual(nextDate(9,30,2019), "10/01/2019"));
+        System.out.println("D3 test: " + checkEqual(nextDate(9, 30, 2019), "10/01/2019"));
         // D4
-        System.out.println("D4 test: " + checkEqual(nextDate(1,31,2020), "02/01/2020"));
+        System.out.println("D4 test: " + checkEqual(nextDate(1, 31, 2020), "02/01/2020"));
         // Y1
-        System.out.println("Y1 test: " + checkEqual(nextDate(02,28,2000), "02/29/2000"));
+        System.out.println("Y1 test: " + checkEqual(nextDate(02, 28, 2000), "02/29/2000"));
         // Y2: Divisible by 400 and 4
-        System.out.println("Y2(divisible by 400 and 4) test: " + checkEqual(nextDate(2,28,2000), "02/29/2000"));
+        System.out.println("Y2(divisible by 400 and 4) test: " + checkEqual(nextDate(2, 28, 2000), "02/29/2000"));
         // Y2: Divisible by only 4
-        System.out.println("Y2(divisible by only 4) test: " + checkEqual(nextDate(2,28,2012), "02/29/2012"));
+        System.out.println("Y2(divisible by only 4) test: " + checkEqual(nextDate(2, 28, 2012), "02/29/2012"));
         // Y3
-        System.out.println("Y3 test: " + checkEqual(nextDate(2,28,2021), "03/01/2021"));
+        System.out.println("Y3 test: " + checkEqual(nextDate(2, 28, 2021), "03/01/2021"));
     }
 
+    // Random Testing
     public static void randomTesting() {
         String expected = "10/15/1996";
         String actual = nextDate(10, 14, 1996);
